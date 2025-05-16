e7db9cc7f2f0bcf8fa4b5f87a0a0e3a018abb895
GjergjiSh <gjergji.shkurti@yahoo.com>
2025-05-16 22:18:24 +0200

    replace with a2l definiton

--- lib/include/histogram.h
+++ lib/include/histogram.h
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
+
+#define NO_OFFSET 0
 
 namespace xcp {
 void capture();
@@ -17,7 +20,7 @@ static constexpr float kF5 = 25.3f;
 static constexpr float kF6 = 25.6f;
 } // namespace mock_periods
 
-template <int64_t Offset, int64_t Step, int64_t NumPoints> class Histogram {
+template <int64_t NumPoints, int64_t BinWidth, int64_t Offset> class Histogram {
 public:
   explicit Histogram() = default;
   ~Histogram() = default;
@@ -27,21 +30,33 @@ public:
   Histogram(Histogram &&) = default;
   Histogram &operator=(Histogram &&) = default;
 
-  void update_period(float period) noexcept;
-  void print(const char *unit) const noexcept {
+  void update_period(float period) noexcept {
+    last_period_ = current_period_;
+    current_period_ = period;
+  };
 
+  void print(const char *unit) const noexcept {
+    const auto verbose = getenv("HISTO_VERBOSE") != nullptr;
+    if (verbose) {
+      printf("Histogram:\n");
+      for (int i = 0; i < NumPoints; ++i) {
+        int start = Offset * BinWidth + i * BinWidth;
+        int end = start + BinWidth;
+        printf("[%d] [%d .. %d[ (%s) (%d)\n", i, start, end, unit, bins_[i]);
+      }
+      printf("Last period: %.2f %s\n", last_period_, unit);
+      printf("Current period: %.2f %s\n", current_period_, unit);
+    } else {
+      for (int i = 0; i < NumPoints; ++i) {
+        printf("%d\n ", bins_[i]);
+      }
+    }
   }
 
 private:
   void calculate_bin_split() const noexcept {}
 
-//   uint32_t bins_[BinCount] = {0};
+  uint32_t bins_[NumPoints] = {0};
   float last_period_ = 0.0f;
   float current_period_ = 0.0f;
 };
-
-// What does each bin represent?
-// A range of periods
-// The periods are numbers
-// We need to calculate which bin a period falls into
-// We need to calculate how to split the BinCount into ranges
--- tests/histogram_tests.cpp
+++ tests/histogram_tests.cpp
@@ -8,15 +8,15 @@ protected:
 };
 
 TEST_F(HistogramTest, ExampleTest) {
-  //  10 bins, each 5ms wide
-  // 0...5ms, 5..10ms, 10..15ms, ..., 45..50ms
-//   Histogram<10, 5> histogram;
-//   histogram.print("ms");
+  Histogram<30, 10, NO_OFFSET> histogram;
+  histogram.print("ms");
+}
 
-  // Car use case
-  // 23.98, 24.0, 24.31, 24,8, 25.0, 25.3 ms
-  // 2398 2400 2431 2480 2500 2530 us | / 2
-  // 1000...2000us, 2000..3000us, 4000..5000us, ..., 4500..5000us
-  // Offset step numpoints
-  // Histogram<3, 10> cycle_time;
+TEST_F(HistogramTest, PrintWithOffset) {
+  Histogram<30, 10, 2> histogram;
+  histogram.print("ms");
+}
+
+TEST_F(HistogramTest, UpdatePeriod) {
+    Histogram<30, 10, NO_OFFSET> histogram;
 }
