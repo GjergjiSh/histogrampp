873d1e498b7fc98a3e5a1f7528b80369452322eb
GjergjiSh <gjergji.shkurti@yahoo.com>
2025-05-16 23:40:52 +0200

    prep for min max

--- lib/include/histogram.h
+++ lib/include/histogram.h
@@ -4,23 +4,7 @@
 #include <cstdio>
 #include <cstdlib>
 
-#define NO_OFFSET 0
-
-namespace xcp {
-void capture();
-void measure();
-} // namespace xcp
-
-namespace mock_periods {
-static constexpr float kF1 = 24.0f;
-static constexpr float kF2 = 24.3f;
-static constexpr float kF3 = 24.6f;
-static constexpr float kF4 = 25.0f;
-static constexpr float kF5 = 25.3f;
-static constexpr float kF6 = 25.6f;
-} // namespace mock_periods
-
-template <int64_t NumPoints, int64_t BinWidth, int64_t Offset> class Histogram {
+template <int64_t BinCount, int64_t BinWidth> class Histogram {
 public:
   explicit Histogram() = default;
   ~Histogram() = default;
@@ -30,33 +14,55 @@ public:
   Histogram(Histogram &&) = default;
   Histogram &operator=(Histogram &&) = default;
 
-  void update_period(float period) noexcept {
-    last_period_ = current_period_;
-    current_period_ = period;
+  void update(double cycle_time) noexcept {
+    const size_t bin_index = static_cast<size_t>(cycle_time / BinWidth);
+
+    if (getenv("HISTO_VERBOSE")) {
+      printf("Updating histogram with cycle time: %.2f, "
+             "bin index: %zu, bin width: %ld\n",
+             cycle_time, bin_index, BinWidth);
+    }
+
+    if (bin_index >= 0 && bin_index < BinCount) {
+      bins_[bin_index]++;
+    } else {
+      printf("Warning: Period %.2f out of bounds for "
+             "histogram with %ld points and bin width %ld\n",
+             cycle_time, BinCount, BinWidth);
+    }
+
+    if (cycle_time < lowest_cycle_time_ || lowest_cycle_time_ == 0) {
+      lowest_cycle_time_ = cycle_time;
+    }
+
+    if (cycle_time > lowers_cycle_time) {
+      lowers_cycle_time = cycle_time;
+    }
+
+    last_cycle_time_ = cycle_time;
   };
 
   void print(const char *unit) const noexcept {
-    const auto verbose = getenv("HISTO_VERBOSE") != nullptr;
-    if (verbose) {
+    if (getenv("HISTO_VERBOSE")) {
       printf("Histogram:\n");
-      for (int i = 0; i < NumPoints; ++i) {
-        int start = Offset * BinWidth + i * BinWidth;
+      for (int i = 0; i < BinCount; ++i) {
+        int start = BinWidth + i * BinWidth;
         int end = start + BinWidth;
         printf("[%d] [%d .. %d[ (%s) (%d)\n", i, start, end, unit, bins_[i]);
       }
-      printf("Last period: %.2f %s\n", last_period_, unit);
-      printf("Current period: %.2f %s\n", current_period_, unit);
+      printf("Last cycle time: %.2f %s\n", last_cycle_time_, unit);
+      printf("Lowest cycle time: %.2f %s\n", lowest_cycle_time_, unit);
+      printf("Highest cycle time: %.2f %s\n", lowers_cycle_time, unit);
     } else {
-      for (int i = 0; i < NumPoints; ++i) {
+      for (int i = 0; i < BinCount; ++i) {
         printf("%d\n ", bins_[i]);
       }
     }
   }
 
 private:
-  void calculate_bin_split() const noexcept {}
-
-  uint32_t bins_[NumPoints] = {0};
-  float last_period_ = 0.0f;
-  float current_period_ = 0.0f;
+  uint32_t bins_[BinCount] = {0};
+  double last_cycle_time_ = 0;
+  double lowest_cycle_time_ = 0;
+  double lowers_cycle_time = 0;
 };
--- tests/histogram_tests.cpp
+++ tests/histogram_tests.cpp
@@ -8,15 +8,33 @@ protected:
 };
 
 TEST_F(HistogramTest, ExampleTest) {
-  Histogram<30, 10, NO_OFFSET> histogram;
+  Histogram<30, 10> histogram;
   histogram.print("ms");
 }
 
 TEST_F(HistogramTest, PrintWithOffset) {
-  Histogram<30, 10, 2> histogram;
+  Histogram<30, 10> histogram;
   histogram.print("ms");
 }
 
 TEST_F(HistogramTest, UpdatePeriod) {
-    Histogram<30, 10, NO_OFFSET> histogram;
+  Histogram<30, 10> histogram;
+
+  // clang-format off
+  std::vector<double> values = {
+    23.0, 23.1, 23.2, 23.3, 23.4,
+    23.5, 23.6, 23.7, 23.8, 23.9,
+    24.0, 24.1, 24.2, 24.3, 24.4,
+    24.5, 24.6, 24.7, 24.8, 24.9,
+    25.0, 25.1, 25.2, 25.3, 25.4,
+    25.5, 25.6, 25.7, 25.8, 25.9,
+    26.0
+  };
+  // clang-format on
+
+  for (const auto &v : values) {
+    histogram.update(v);
+  }
+
+  histogram.print("ms");
 }
