3ab61808a5fc5033a037f06326f4f3bcd42bf267
GjergjiSh <gjergji.shkurti@yahoo.com>
2025-05-17 14:40:44 +0200

    Refactor main function to use a lambda for histogram generation and printing

--- main/main.cpp
+++ main/main.cpp
@@ -1,18 +1,25 @@
 #include "histogram.h"
 #include <random>
 
-#include <unistd.h>
-
 int main() {
-  Histogram<2300, 2600, 10> histogram;
-  std::mt19937 gen(std::random_device{}());
-  std::normal_distribution<double> normal_dist(2450.0, 25.0);
+  // clang-format off
+  auto histogram_example = [](
+    auto &&histogram, double mean, double stddev,
+    uint64_t samples, const char *unit) {
 
-  for (uint64_t i = 0; i < 1000000; ++i) {
-    double sample = normal_dist(gen);
-    microseconds cycle_time_us = static_cast<microseconds>(sample);
-    histogram.Update(cycle_time_us);
-  }
+    std::mt19937 gen(std::random_device{}());
+    std::normal_distribution<double> normal_dist(mean, stddev);
 
-  histogram.Print("us");
+    for (uint64_t i = 0; i < samples; ++i) {
+      double sample = normal_dist(gen);
+      microseconds cycle_time_us = static_cast<microseconds>(sample);
+      histogram.Update(cycle_time_us);
+    }
+
+    histogram.Print(unit);
+  };
+  // clang-format on
+
+  histogram_example(Histogram<2300, 2600, 10>(), 2450.0, 25.0, 1000000, "us");
+  histogram_example(Histogram<9800, 10000, 10>(), 9900.0, 25.0, 1000000, "us");
 }
